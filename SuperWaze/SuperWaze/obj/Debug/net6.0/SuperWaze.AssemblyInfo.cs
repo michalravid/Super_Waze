e5cd185c4b710e914eb1997508cd9898e1c3578b
A <mr0527601091@gmail.com>
2024-07-24 04:41:58 +0300

    Second commit --all project with middleware_JWT

--- SuperWaze/SuperWaze/obj/Debug/net6.0/SuperWaze.AssemblyInfo.cs
+++ SuperWaze/SuperWaze/obj/Debug/net6.0/SuperWaze.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperWaze")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6b54253e43dbd45068e6e3f9970b5f91af81ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a1e68826a7856d6088867cf570140b494b6646b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperWaze")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperWaze")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
