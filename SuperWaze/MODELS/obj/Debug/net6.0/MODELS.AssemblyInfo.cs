6f0fc1d23c48c3b4c57df583f9aaf80b38fa1dc2
A <mr0527601091@gmail.com>
2024-07-24 06:42:56 +0300

    Adding all project files to new branch

--- SuperWaze/MODELS/obj/Debug/net6.0/MODELS.AssemblyInfo.cs
+++ SuperWaze/MODELS/obj/Debug/net6.0/MODELS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MODELS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5884f6aaf85c264a89b210a406d8382967abf57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403361cabc808335dcb1a4bcf9c877c2abf52375")]
 [assembly: System.Reflection.AssemblyProductAttribute("MODELS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MODELS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
